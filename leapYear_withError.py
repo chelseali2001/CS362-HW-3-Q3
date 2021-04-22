9b9eee22f5fa963fb24273f2c3ca2450c141c255
Chelsea Li <chelseali2001@gmail.com>
2021-04-21 22:58:21 -0700

    created leap year program with error checking

--- leapYear_withError.py
+++ leapYear_withError.py
@@ -1,1 +1,21 @@
+valid = 0
 
+while valid == 0:
+    valid = 1
+    n = input("Enter a year: ")
+
+    for x in n:
+        if x < '0' or x > '9':
+            print("Error: invalid input, ints only.")
+            valid = 0
+            break
+
+n = int(n)
+
+if n % 4 == 0:
+    if n % 100 == 0 and n % 400 != 0:
+        print(n, "is not a leap year")
+    else:
+        print(n, "is a leap year")
+else:
+    print(n, "is not a leap year")
